e8349c2e054a1588dd076c750cf6cf3274ff5659
badrandom <denis.varihanov@ya.ru>
2022-07-12 10:33:35 +0300

    Fixed links and version

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,1 +1,10 @@
-VERSION = '0.1.0'
+# Current version of TestGuru
+
+module Version
+  def current_version
+    '0.1.0'
+  end
+end
+
+# Метод будет вызываться из классов с этой примесью. Если в процессе понадобится класс в модуле или вызов по типу
+# Version.current_version, то добавлю self. или, собственно, класс.
